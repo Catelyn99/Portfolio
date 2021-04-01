bd52c342b735b4794777021b465a27376f754ec3
Catelyn99 <katarzynasroka99@gmail.com>
2021-04-01 22:28:09 +0200

    navigation to projects

--- src/Projects/Project/Project.jsx
+++ src/Projects/Project/Project.jsx
@@ -2,11 +2,11 @@ import './Project.css'
 
 export default function Project(props) {
     return (
-        <article className="project-container">
-            <header className="project-name">{props.project.name}</header>
+        <a href={props.project.url} className="project-container">
             <div className="project-image-container">
                 <img src={props.project.img.src} alt={props.project.img.alt} />
             </div>
+            <h2 className="project-name">{props.project.name}</h2>
             <div className="project-tech-description">
             <div className="project-tech-used">
                 Użyte technologie:
@@ -15,6 +15,6 @@ export default function Project(props) {
             </div>
             <div className="project-description">{props.project.description}</div>
             </div>
-        </article>
+        </a>
     )
 }
