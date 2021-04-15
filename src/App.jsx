e69531a0e8674964134338a8bcdd9b8ae40b1192
Catelyn99 <katarzynasroka99@gmail.com>
2021-04-15 15:39:41 +0200

    changed responsive

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,7 @@ const App = () => {
         scrollToCv={scrollToCv}
         scrollToProjects={scrollToProjects} />
       <div className={styles.container}>
-        <div className={styles.introduction}>
-          Front-end Developer
-        </div>
+        
         <div className={styles.section} ref={refAboutMe}>
           <AboutMe />
         </div>
