35786b0cb4ad0428925b42cb39a6dfbb3a5c39ca
Catelyn99 <katarzynasroka99@gmail.com>
2021-04-15 20:35:33 +0200

    changed photo

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,17 +4,17 @@ export default function Header(props) {
 
     return (
         <div className={styles.container}>
-        <header>
-        <div className={styles.pagename}>Katarzyna Sroka</div>
-        <div className={styles.pagenamemobile}>KS</div>
-        <div className={styles.introduction}>Front-end Developer</div>
-        </header>
-        <section>
-            <ul className={styles.menu}>
-                <li onClick={props.scrollToAboutMe} className={styles.menuitem}>O MNIE </li>
-                <li onClick={props.scrollToCv} className={styles.menuitem}>CV</li>
-                <li onClick={props.scrollToProjects} className={styles.menuitem}>PROJEKTY</li>
-            </ul>
+            <header>
+                <div className={styles.pagename}>Katarzyna Sroka</div>
+                <div className={styles.pagenamemobile}>KS</div>
+                <div className={styles.introduction}>Front-end Developer</div>
+            </header>
+            <section>
+                <ul className={styles.menu}>
+                    <li onClick={props.scrollToProjects} className={styles.menuitem}>PROJEKTY</li>
+                    <li onClick={props.scrollToAboutMe} className={styles.menuitem}>O MNIE </li>
+                    <li onClick={props.scrollToCv} className={styles.menuitem}>CV</li>
+                </ul>
             </section>
         </div>
     );
